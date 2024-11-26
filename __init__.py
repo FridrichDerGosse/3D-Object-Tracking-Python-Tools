a9cabc7b3343414b89a6526ee6f4bc4bd4a81e54
Nilusink <nilusink@protonmail.com>
2024-11-26 16:00:07 +0100

    debugging and stuff

--- __init__.py
+++ __init__.py
@@ -2,3 +2,5 @@ from ._combined_result import CombinedResult
 from ._data_types import Box, AngularTrack
 from ._tracking import Track, TrackUpdate
 from ._vectors import Vec2, Vec3
+from .debugging import *
+from .logic import *
---
+++ logic/__init__.py
@@ -0,0 +1,2 @@
+from ._utility_classes import BetterDict, SimpleLock
+from ._utility_functions import classname
